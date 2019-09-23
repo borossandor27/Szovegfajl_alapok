44091cf39fe98d48127b605224c0d472fcd2d056
Beregszászi Pál Szakközépiskola <bpszki@outlook.hu>
2019-09-23 06:43:52 +0200

    Hozzáfűzéssel kibővítve

--- Szovegfajl_alapok/Program.cs
+++ Szovegfajl_alapok/Program.cs
@@ -5,106 +5,152 @@ using System.Collections.Generic;
 
 namespace Szovegfajl_alapok
 {
-    class Program
+    class Tanulo
     {
-        class Tanulo
+        /*
+         * -- logikailag összetartozó adatok tárolására legrugalmasabb adatszerkezet
+         */
+        private string kod; //-- A tanuló kódja (négyjegyű szám)
+        public string Kod
         {
-            /*
-             * -- logikailag összetartozó adatok tárolására legrugalmasabb adatszerkezet
-             */
-            private string kod; //-- A tanuló kódja (négyjegyű szám)
-            public string Kod
+            get
             {
-                get
-                {
-                    return kod;
-                }
-                set
-                {
-                    int ertek;
-                    if (int.TryParse(value, out ertek) && ertek >= 1000 && ertek <= 9999)
-                    {
-                        kod = value;
-                    }
-                    else
-                    {
-                        kod = "0000";
-                    }
-                }
-
+                return kod;
             }
-            private string nev;
-            public string Nev //-- A tanuló neve  (min=3 && max=35)
+            set
             {
-                get
+                int ertek;
+                if (int.TryParse(value, out ertek) && ertek >= 1000 && ertek <= 9999)
                 {
-                    return nev;
+                    kod = value;
                 }
-                set
+                else
                 {
-                    int hossz = value.Length;
-                    if (hossz >= 3 && hossz <= 35)
-                    {
-                        nev = value;
-                    }
-                }
-            }
-            private DateTime szuletett; //-- születési idő (maximum 100 éves lehet)
-            public string Szuletett
-            {
-                get
-                {
-                    return szuletett.ToString("yyyy.MM.dd");
-                }
-                set
-                {
-                    DateTime d = DateTime.Parse(value);
-
-                    if (DateTime.TryParse(value, out d) && d.Year >= DateTime.Today.Year - 100 && d.Year <= DateTime.Today.Year)
-                    {
-                        szuletett = DateTime.Parse(value);
-                    }
-                    else
-                    {
-                        szuletett = new DateTime();
-                    }
+                    kod = "0000";
                 }
             }
 
-            public string getTanulo()
+        }
+        private string nev;
+        public string Nev //-- A tanuló neve  (min=3 && max=35)
+        {
+            get
             {
-                return string.Join(";", this.Kod, this.Nev, this.Szuletett);
+                return nev;
             }
-
-            public Tanulo(string a, string b, string c)
+            set
             {
-                this.Kod = a;
-                this.Nev = b;
-                this.Szuletett = c;
+                int hossz = value.Length;
+                if (hossz >= 3 && hossz <= 35)
+                {
+                    nev = value;
+                }
             }
         }
+        private DateTime szuletett; //-- születési idő (maximum 100 éves lehet)
+        public string Szuletett
+        {
+            get
+            {
+                return szuletett.ToString("yyyy.MM.dd");
+            }
+            set
+            {
+                DateTime d = DateTime.Parse(value);
+
+                if (DateTime.TryParse(value, out d) && d.Year >= DateTime.Today.Year - 100 && d.Year <= DateTime.Today.Year)
+                {
+                    szuletett = DateTime.Parse(value);
+                }
+                else
+                {
+                    szuletett = new DateTime();
+                }
+            }
+        }
+
+        public string getTanulo()
+        {
+            //-- Szövegfájlba való íráshoz ";"-vel összefűzve adja az adatokat
+            return string.Join(";", this.Kod, this.Nev, this.Szuletett);
+        }
+
+        public Tanulo(string a, string b, string c)
+        {
+            this.Kod = a;
+            this.Nev = b;
+            this.Szuletett = c;
+        }
+    }
+    class Program
+    {
+        static List<Tanulo> tanulok = new List<Tanulo>();              //-- adatok a gyakorláshoz
+
         static void Main(string[] args)
         {
             /*
              * -- Szövegfájlba írás ----
              */
 
-            List<Tanulo> tanulok = new List<Tanulo>();              //-- adatok a gyakorláshoz
             tanulok.Add(new Tanulo("1000", "Béla", "2018-11-11"));
             tanulok.Add(new Tanulo("1001", "Ferenc", "2009-01-07"));
             tanulok.Add(new Tanulo("1002", "Ágnes", "2008-09-01"));
             tanulok.Add(new Tanulo("1003", "Ibolya", "2008-10-06"));
             tanulok.Add(new Tanulo("1004", "Aladár", "2008-12-15"));
-            //-- gyakorló adatok kiiratása ---
+            /*
+             * -- gyakorló adatok kiiratása a képernyőre ---
+             */
             foreach (Tanulo item in tanulok)
             {
                 Console.WriteLine("{0,-8} {1,-20} {2,15}", item.Kod, item.Nev, item.Szuletett);
             }
             string fajl = "tanulok.txt"; //-- ha nincs elérési út, akkor az exe állomány mappájában hozza létre
-
-            //-- kiiras fájlba ---
             string TeljesEleresiUt = Directory.GetCurrentDirectory() + @"\" + fajl;
-            Console.WriteLine("Adatok kiírása {0} fájlba...", TeljesEleresiUt);
+            if (KiirasFajlba(TeljesEleresiUt))
+            {
+                /************************
+                 *                      *
+                 *      Hozzáfúzés      *
+                 *                      *
+                 ************************/
+                Console.WriteLine("\nÚjabb adatok hozzáírása a fájlhoz...\n");
+                int MeglevoAdatokSzama = tanulok.Count;
+                tanulok.Add(new Tanulo((1000 + MeglevoAdatokSzama).ToString("0000"), "Adél", "2008-2-19"));
+                tanulok.Add(new Tanulo((1000 + MeglevoAdatokSzama + 1).ToString("0000"), "Eleonóra", "2008-3-15"));
+                StreamWriter sw = null;
+                try
+                {
+                    using (sw = new StreamWriter(TeljesEleresiUt, true))
+                    {
+                        for (int i = MeglevoAdatokSzama; i < tanulok.Count; i++)
+                        {
+                            sw.WriteLine(tanulok[i].getTanulo());
+                        }
+                    }
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine(e.Message);
+                    throw;
+                } finally
+                {
+                    if (sw!=null)
+                    {
+                        sw.Close();
+                        sw.Dispose();
+                    }
+                }
+            }
+
+            Console.WriteLine("Program vége");
+            Console.ReadKey();
+        }
+
+        static bool KiirasFajlba(string fajl)
+        {
+            bool sikerult = false;
+            //-- kiiras fájlba ---
+            Console.WriteLine("\nAdatok kiírása\n\n\t{0} \n\nfájlba...\n", fajl);
             StreamWriter sw = null;
             try
             {
@@ -114,26 +160,24 @@ namespace Szovegfajl_alapok
                     {
                         sw.WriteLine(item.getTanulo());
                     }
-                    sw.Close();
                 }
                 Console.WriteLine("Szövegfájl létrehozás vége.");
+                sikerult = true;
             }
-            catch (Exception e)
+            catch (IOException e)
             {
-                Console.WriteLine("{0} hiba a {1} fájl létrehozása közben!", e.Message, TeljesEleresiUt);
+                Console.WriteLine("{0} hiba a {1} fájl létrehozása közben!", e.Message, fajl);
                 throw;
             }
             finally
             {
                 if (sw != null)
                 {
-                    sw.Flush();
                     sw.Close();
                     sw.Dispose();
                 }
             }
-            Console.WriteLine("Program vége");
-            Console.ReadKey();
+            return sikerult;
         }
     }
 }
